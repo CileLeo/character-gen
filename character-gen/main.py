6a3aa36d4466436dbbd03a22bf5d9733bf36b0db
Leo <60359673+CileLeo@users.noreply.github.com>
2021-05-27 11:54:58 +0200

    what the fuck

--- character-gen/main.py
+++
@@ -1,9 +0,0 @@
-import character_gen
-
-character = character_gen.generate()
-
-print('age = %i' % (character.age))
-print('home country = %s' % (character.homeCountry))
-print('ethnicity = %s' % (character.ethnicity))
-print('gender = %s' % (character.gender))
-print('name = %s %s' % (character.firstName, character.lastName))
